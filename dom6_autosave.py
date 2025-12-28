63df1134335ef6fe5d1a575d04e8393f40e61f84
laodie666 <mhkjordan@gmail.com>
2025-12-28 21:34:19 +0800

    The words are actually understandable

--- dom6_autosave.py
+++ dom6_autosave.py
@@ -32,9 +32,7 @@ def get_turn_number(filepath):
         
     with open(filepath, "rb") as f:
         f.seek(14)
-        
         chunk = f.read(2)
-        
         return struct.unpack('<H', chunk)[0]
 
 
@@ -43,64 +41,55 @@ def save_turn(game_index):
     game_name = game_save_list[game_index][0]
     src_path = game_save_list[game_index][1]
     dst_path = os.path.join(backup_path, game_save_list[game_index][0])
-    print(f"Saving {game_name}")
+    print(f"\nSaving: {game_name}\n")
     
-    # First find the directory for the game in backup folder, create if doesn't exist
     if not os.path.exists(dst_path):
         os.makedirs(dst_path)
     
-    # Copy over everything that is not trn or 2h. 
-    shutil.copytree(src_path, dst_path, ignore = shutil.ignore_patterns('*.trn', '*.2h'), dirs_exist_ok = True)
-    
-    # Find the turn number
-    # TURN_NUMBER=$(hexdump -n 2 -s 14 -e '1/2 "%d"' $TURN_FILE) This is the turn number in the trn file. 2 bytes at the 14th offset. 
-    # @ben_sphynx told me about this on Reddit Dominions
-    # Err if trn file or 2h doesn't exist
+    shutil.copytree(src_path, dst_path, ignore=shutil.ignore_patterns('*.trn', '*.2h'), dirs_exist_ok=True)
     
     turn_number = -1
     
     for file in os.listdir(src_path):
-            if file.endswith('.trn'):
-                # print whole path of files
-                turn_number = get_turn_number(os.path.join(src_path, file))
-                break
+        if file.endswith('.trn'):
+            turn_number = get_turn_number(os.path.join(src_path, file))
+            break
     
     if turn_number == -1:
-        print('Reading turn number from trn file failed.')
+        print("\nFailed to read turn number from .trn file\n")
         return
     
-    # Copy the trn and 2h files into the turn specific folder 
-    # Overwriting existing turn.
-    
-    # Copy the trn and 2h files into the turn specific folder 
-    # Overwriting existing turn.
-    print("1 Save name is current turn number")
-    print("2 Enter save name")
-    user_input = input()
+    print("Choose save naming option:")
+    print("1 Use current turn number")
+    print("2 Enter custom save name\n")
+    user_input = input("Select option (1–2): ")
+    print()
     
     if not user_input.isdigit():
-        print("Please input an interger")
+        print("Invalid input: please enter a number\n")
         return
         
     user_input = int(user_input)
     if user_input == 1:    
         save_name = f"turn_{turn_number}"
     elif user_input == 2:
-        save_name = input("Enter save name: ")
+        save_name = input("Enter custom save name: ")
+        print()
     else:
-        print("Invalid save name.")
+        print("Invalid selection\n")
+        return
     
     save_path = os.path.join(dst_path, save_name)
     if save_name in os.listdir(dst_path):
-        print("overwriting existing turn")
+        print("Overwriting existing save\n")
         shutil.rmtree(save_path)
         
     os.makedirs(save_path)
     
     for name in os.listdir(src_path):
         if name.endswith('.trn') or name.endswith('.2h'):
-            shutil.copy2(os.path.join(src_path, name), 
-                        os.path.join(save_path, name))
+            shutil.copy2(os.path.join(src_path, name),
+                         os.path.join(save_path, name))
     return
 
 
@@ -110,8 +99,8 @@ def load_turn(game_index):
     src_path = game_backup_list[game_index][1] 
     dst_path = os.path.join(game_save_path, game_name)
     
-    print(f"Loading {game_name}")
-    # Ask for save to load, print all the ones available
+    print(f"\nLoading: {game_name}\n")
+    
     saves = []
     for name in os.listdir(src_path):
         if os.path.isdir(os.path.join(src_path, name)):
@@ -120,33 +109,34 @@ def load_turn(game_index):
     for i in range(len(saves)):
         print(i, saves[i])
     
-    user_input = input("Select save to load: ")
+    print()
+    user_input = input("Select backup to load: ")
+    print()
+    
     if not user_input.isdigit():
-        print("Please input an interger")
+        print("Invalid input: please enter a number\n")
         return
     
     user_input = int(user_input)
     
     if game_name in os.listdir(game_save_path):
-        print("overwriting existing turn")
+        print("Overwriting existing save\n")
         shutil.rmtree(dst_path)
         
     os.mkdir(dst_path)
     
-    # copy over all the stuff in the folder
     for file in os.listdir(src_path):
         if not os.path.isdir(os.path.join(src_path, file)):
-            shutil.copy2(os.path.join(src_path, file), 
-                        os.path.join(dst_path, file))
+            shutil.copy2(os.path.join(src_path, file),
+                         os.path.join(dst_path, file))
     
-    # copy over the 2h and trn file in the turn folder
     save_name = saves[user_input]
     save_path = os.path.join(src_path, save_name)
     for file in os.listdir(save_path):
-        shutil.copy2(os.path.join(save_path, file), 
-                        os.path.join(dst_path, file))
-    
+        shutil.copy2(os.path.join(save_path, file),
+                     os.path.join(dst_path, file))
     return
+
 
 def delete_backup_save(game_index):
     src_path = game_backup_list[game_index][1]
@@ -158,9 +148,12 @@ def delete_backup_save(game_index):
     for i in range(len(saves)):
         print(i, saves[i])
     
-    user_input = input("Select save to delete: ")
+    print()
+    user_input = input("Select backup to delete: ")
+    print()
+    
     if not user_input.isdigit():
-        print("Please input an interger")
+        print("Invalid input: please enter a number\n")
         return
     
     user_input = int(user_input)
@@ -175,122 +168,101 @@ def delete_backup_save(game_index):
     if not has_save:
         shutil.rmtree(src_path)
 
+
 if __name__ == "__main__":
 
     if not os.path.exists(backup_path):
         os.makedirs(backup_path)
     
-     
-    
     while True:
     
-        # Title
         print(r"""
     ________                   ________   _________                         
     \______ \   ____   _____  /  _____/  /   _____/____ ___  __ ___________ 
-    |    |  \ /  _ \ /     \/   __  \   \_____  \\__  \\  \/ // __ \_  __ \
-    |_   `   (  <_> )  Y Y  \  |__\  \  /        \/ __ \\   /\  ___/|  | \/
+     |    |  \ /  _ \ /     \/   __  \   \_____  \\__  \\  \/ // __ \_  __ \
+     |_   `   (  <_> )  Y Y  \  |__\  \  /        \/ __ \\   /\  ___/|  | \/
     /_______  /\____/|__|_|  /\_____  / /_______  (____  /\_/  \___  >__|   
             \/             \/       \/          \/     \/          \/                                                                                   
-                                                                                                                                                            
                 """)   
         
         game_list = get_save_dirs()
         game_backup_list = get_backup_dirs()
 
-        print("0 Create backup save")
-        print("1 Load backup save")
-        print("2 Delete backup save")
-        print("3 Exit")
+        print("Main menu:")
+        print("0 Create backup")
+        print("1 Load backup")
+        print("2 Delete backup")
+        print("3 Exit\n")
         
-        user_input = input("Select option: ")
-        print("--------------------------------")
+        user_input = input("Select option (0–3): ")
+        print("\n--------------------------------\n")
+        
         if not user_input.isdigit():
-            print("Please input an interger")
+            print("Invalid input: please enter a number\n")
             continue
         
         user_input = int(user_input)
         if user_input not in range(0, 4):
-            print("invalid input")
+            print("Invalid selection\n")
             continue
-        
-        #Save sequence
         
         if user_input == 0:
             for i in range(len(game_list)):
                 print(i, game_list[i][0])
             
-            user_input = input("Select game to save: ")
+            print()
+            user_input = input("Select game to back up: ")
+            print()
+            
             if not user_input.isdigit():
-                print("Please input an interger")
+                print("Invalid input: please enter a number\n")
                 continue
                 
             user_input = int(user_input)
             if user_input not in range(0, len(game_list)):
-                print("No such game")
+                print("Invalid selection\n")
                 continue
             
             save_turn(user_input)
         
-        # Load Sequence
         elif user_input == 1:
             for i in range(len(game_backup_list)):
                 print(i, game_backup_list[i][0])
             
+            print()
             user_input = input("Select game to load: ")
+            print()
+            
             if not user_input.isdigit():
-                print("Please input an interger")
+                print("Invalid input: please enter a number\n")
                 continue
                 
             user_input = int(user_input)
             if user_input not in range(0, len(game_backup_list)):
-                print("No such game")
+                print("Invalid selection\n")
                 continue
 
             load_turn(user_input)
         
-        
-        # Delete Save Sequence
         elif user_input == 2:
             game_backup_list = get_backup_dirs()
             for i in range(len(game_backup_list)):
                 print(i, game_backup_list[i][0])
             
-            user_input = input("Select game to delete backup save for: ")
+            print()
+            user_input = input("Select game to delete backups for: ")
+            print()
+            
             if not user_input.isdigit():
-                print("Please input an interger")
+                print("Invalid input: please enter a number\n")
                 continue
                 
             user_input = int(user_input)
             if user_input not in range(0, len(game_backup_list)):
-                print("No such game")
+                print("Invalid selection\n")
                 continue
             
             delete_backup_save(user_input)
         
-        else: 
+        else:
             exit()
-        
-        
-    
-    
-    # TODO 
-    # Log changes in folders, if the folder changed has different turn number that's not recorded, save the turn. 
-    ################# Observe changes, autosave ######################
-    # logging.basicConfig(level=logging.INFO,
-    #                     format='%(asctime)s - %(message)s',
-    #                     datefmt='%Y-%m-%d %H:%M:%S')
-
-    # event_handler = LoggingEventHandler()
-
-    # observer = Observer()
-    # observer.schedule(event_handler, save_path, recursive = True)
-
-    # observer.start()
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     observer.stop()
-    # observer.join()
-    
